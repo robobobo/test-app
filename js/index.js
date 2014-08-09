bce1fa70059ab430a1cc40c6de55f0275357e471
Robert Boland <robertboland1@gmail.com>
2014-08-09 19:06:15 +0100

    test

--- js/index.js
+++ js/index.js
@@ -34,6 +34,8 @@ var app = {
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
+        alert("ready");
+        console.log(navigator.camera)
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
